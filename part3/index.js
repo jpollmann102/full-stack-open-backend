e9acb00419cfc5456803a9e5a074c094b43e1cfb
Josh Pollmann <joshpollmann102@gmail.com>
2021-03-19 13:46:49 -0400

    Part 3, 3.9 and 3.11

--- part3/index.js
+++ part3/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(morgan('tiny'));
+app.use(express.static('build'));
 
 let numbers = [
   {
@@ -110,7 +113,7 @@ app.post('/api/persons', (request, response) => {
   response.json(number);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
